8198e1ca58a156ae22bef223285ac9fd38b9c37a
Faizal <nightowlclub72@gmail.com>
2026-06-21 05:09:09 +0000

    first scene

--- main.py
+++ main.py
@@ -1,1 +1,44 @@
+import math 
+
+print ("\n Welcome to the MYSTERY DIARIES........\n")
+
+name = input("Enter your name : ")
+
+print (f"\n Welcome Dr. {name} !!")
+
+print ("\n\n----- A Murder Was Occured at birmingham high Library......")
+
+
+def setting():
+    print ("\n\n ---- Crime Scene ----")
+    print("\n Year: 1987\n")
+    print(" Location: Blackwood manor! an old mansion isolated in apalachians mounutians.. \n")
+
+    print(" A storm has knocked. out the phone lines and everyone inside is trapped for a night .\n")
+
+    print(" At 11:47, milliomaire business man Edward blackwood found dead in his libraby ")
+    print("\nThe doors are locked.. ")
+
+    print("\nNo One can leave...")
+    
+    print("\nMurderer is still inside the Manor...")
+
+
+
+
+def victim():
+    print("\n--- VICTIM ---")
+
+    print("\n>>> Edward Blackwood")
+    print("\nAge : 68")
+
+    print("\nOwner of Blackwood Industries")
+
+    print("\nKnown for rich, secretive adn hated by Many..")
+    print("\nCause of Death : @ Stab wound")
+    
+
+
+setting()
+
 
